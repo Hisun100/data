597fe0c0d752c807e12a2f02c0b4a4fee50c6ae3
Hisun100 <2313077400@qq.com>
2018-05-11 10:14:07 +0800

    Update demo02_bubbleSorted.py
    完善了注释信息，以便于对嵌套的for语句的理解

--- demo02_bubbleSorted.py
+++ demo02_bubbleSorted.py
@@ -1,5 +1,6 @@
 """
 冒泡排序
+排序算法默认升序
 https://zh.wikipedia.org/wiki/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F
 """
 
@@ -9,7 +10,7 @@ def bubble_sorted(iterable):
     list_len = len(new_list)  #列表中的元素个数
     
     for i in range(list_len - 1): 
-        for j in range(list_len - 1, i, -1):
+        for j in range(list_len - 1, i, -1): ##对剩余元素继续进行冒泡排序
             #升序，比较相邻的元素，若左边元素大于右边元素，则两者交换位置
             if new_list[j] < new_list[j -1]:
                 new_list[j], new_list[j - 1] = new_list[j - 1], new_list[j]
